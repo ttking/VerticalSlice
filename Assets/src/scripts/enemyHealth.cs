e6b9037dada9f1d7a2416a7d8d0a160f7309eb09
TheJerma45 <Jermaine1999@live.nl>
2016-11-23 17:10:38 +0100

    FireFlyMovement
    Firefly Movement and enemy startingcode

---
+++ Assets/src/scripts/enemyHealth.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyHealth : MonoBehaviour {
+
+    public float HealthPoints;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
