69120679c2f8ec9a7b63b8e1e382209bf9379090
Ralph Otte <17785@ma-web.nl>
2016-11-23 15:41:44 +0100

    EntityData + SkillsData Classes

---
+++ Assets/src/scripts/CharacterData.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterData : MonoBehaviour {
+
+    private List<SkillData> skills = new List<SkillData>();
+
+    //Personal Stats
+    private float health;
+    private float mana;
+    private float defense;
+    private float magicDefense;
+    private float attackPower;
+    private float spellPower;
+    private float criticalChance;
+    private float dodgeChance;
+    private float speed;
+
+    private float remainingTimeForAction;
+
+    public float RemainingTimeForAction
+    {
+        get { return remainingTimeForAction; }
+    }
+
+	void Start () {
+        SkillData normalAttack = new SkillData("Normal Attack", Skills.NormalAttack, 1f);
+        AddSkill(normalAttack);
+        SkillData defend = new SkillData("Defend", Skills.Defend, 1f);
+        AddSkill(defend);
+        Debug.Log(skills[0].Skill);
+	}
+
+    public void AddSkill(SkillData skill)
+    {
+        skills.Add(skill);
+    }
+    
+}
---
+++ Assets/src/scripts/Enemy.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : CharacterData {
+
+    void AttackPlayer()
+    {
+
+    }
+}
