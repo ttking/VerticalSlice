1485f09df1eba5d32f3c2bba96de06792015c6f0
TheJerma45 <Jermaine1999@live.nl>
2016-11-28 08:55:18 +0100

    UnityPackage
    +Added Unity Package

--- Assets/src/scripts/Targeting.cs
+++ Assets/src/scripts/Targeting.cs
@@ -1,22 +1,85 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Targeting : MonoBehaviour {
 
     [SerializeField]
-    private GameObject[] Enemies;
-    private GameObject SelectedEnemy;
+    private List<Transform> Enemies;
+    [SerializeField]
+    public Transform SelectedEnemy;
+    private Transform myTransform;
     
      
 
 	// Use this for initialization
 	void Start ()
     {
-	    
+        Enemies = new List<Transform>();
+        SelectedEnemy = null;
+        myTransform = transform;
+
+        AddAllEnemies();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+    public void AddAllEnemies()
+    {
+        GameObject[] go =
+        GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject enemy in go)
+        {
+            AddTarget(enemy.transform);
+        }
+    }
+    
+    public void AddTarget(Transform enemy)
+    {
+        Enemies.Add(enemy);
+    }
+
+    private void SortTargetsByDistance()
+    {
+        Enemies.Sort(delegate (Transform t1, Transform t2)
+            {
+                return Vector3.Distance(t1.position, myTransform.position).CompareTo(Vector3.Distance(t2.position, myTransform.position));
+            });
+    }
+
+    private void TargetEnemy()
+    {
+        if (SelectedEnemy == null)
+        {
+            SortTargetsByDistance();
+            SelectedEnemy = Enemies[0];
+        }
+        else
+        {
+            int index = Enemies.IndexOf(SelectedEnemy);
+
+            if (index < Enemies.Count - 1)
+            {
+                index++;
+            }
+            else
+            {
+                index = 0;
+            }
+            SelectedEnemy = Enemies[index];
+        }
+        SelectEnemy();
+      }
+
+    private void SelectEnemy()
+{
+        //Indicator that the enemy is selected
+}
+
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            TargetEnemy();
+        }
 	}
 }
--- Assets/src/scripts/enemyHealth.cs
+++ Assets/src/scripts/enemyHealth.cs
@@ -3,15 +3,27 @@ using System.Collections;
 
 public class EnemyHealth : MonoBehaviour {
     [SerializeField]
-    private float HealthPoints;
+    private int HealthPoints;
+    private int MinHP = 0;
+    private int MaxHP = 100;
+    private Targeting targeting;
 
-	// Use this for initialization
-	void Start () {
-	
+
+    // Use this for initialization
+    void Start () {
+        Targeting targeting = GetComponent<Targeting>();
+        HealthPoints = MaxHP;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            HealthPoints -= 50;
+        }
+        if (HealthPoints <= MinHP)
+        {
+            Destroy(gameObject);
+        }
 	}
 }
